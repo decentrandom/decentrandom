7e2ee811dcba315de6d811edefd44a99135ff740
DecentRandom <misskiwi@decentrandom.com>
2019-03-05 05:49:05 +0900

    Update app.go

--- app.go
+++ app.go
@@ -1,1 +1,9 @@
 package app
+
+import (
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/tendermint/tendermint/libs/log"
+
+	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	dbm "github.com/tendermint/tendermint/libs/db"
+)
