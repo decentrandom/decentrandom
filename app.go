85c9a562bc0114b76ddd4eb0dafce75aabfef738
DecentRandom <misskiwi@decentrandom.com>
2019-03-05 09:27:54 +0900

    Update app.go

--- app.go
+++ app.go
@@ -1,8 +1,14 @@
 package app
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/params/"
+	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/decentrandom/decentrandom/x/rand"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	dbm "github.com/tendermint/tendermint/libs/db"
