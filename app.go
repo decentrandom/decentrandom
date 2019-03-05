5a0e0dc376557668e1c0a0634c585d6abebdc892
DecentRandom <misskiwi@decentrandom.com>
2019-03-05 09:37:55 +0900

    Update app.go

--- app.go
+++ app.go
@@ -11,5 +11,21 @@ import (
 	"github.com/decentrandom/decentrandom/x/rand"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	dbm "github.com/tendermint/tendermint/libs/db"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
+
+const (
+	appName = "decentrandom"
+)
+
+type decentRandomApp struct {
+	*bam.BaseApp
+	cdc *codec.Codec
+
+	accountKeeper auth.AccountKeeper
+	bankKeeper    bank.Keeper
+}
