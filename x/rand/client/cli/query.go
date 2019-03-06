b80718d642e144e9941803da4180b7d19936afdf
DecentRandom <misskiwi@decentrandom.com>
2019-03-07 02:43:34 +0900

    Create query.go

---
+++ x/rand/client/cli/query.go
@@ -0,0 +1,15 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/spf13/cobra"
+)
+
+/*
+to-do
+*/
