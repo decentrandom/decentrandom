3e5e78ae01a6a588d740c11addbaa63d11914752
DecentRandom <misskiwi@decentrandom.com>
2019-03-06 17:49:16 +0900

    Create querier.go

---
+++ x/rand/querier.go
@@ -0,0 +1,18 @@
+package rand
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+const (
+/*
+	to-do
+*/
+)
+
+/*
+to-do
+*/
