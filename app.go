dd4968a5a1b50a392cda95b0a8f9cbb5808708a7
DecentRandom <misskiwi@decentrandom.com>
2019-03-06 09:48:42 +0900

    Update app.go

--- app.go
+++ app.go
@@ -2,22 +2,21 @@ package app
 
 import (
 	"github.com/cosmos/cosmos-sdk/cmd/gaia/app"
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/cosmos/cosmos-sdk/x/slashing"
-	"github.com/cosmos/cosmos-sdk/x/staking"
-	"github.com/tendermint/tendermint/libs/log"
+	"github.com/cosmos/cosmos-sdk/x/stake"
+	"github.com/tendermint/tendermint/tree/master/libs/log"
 
 	"github.com/decentrandom/decentrandom/x/rand"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	abci "github.com/tendermint/tendermint/abci/types"
-	cmn "github.com/tendermint/tendermint/libs/common"
-	dbm "github.com/tendermint/tendermint/libs/db"
-	tmtypes "github.com/tendermint/tendermint/types"
+	abci "github.com/tendermint/tendermint/tree/master/abci/types"
+	cmn "github.com/tendermint/tendermint/tree/master/libs/common"
+	dbm "github.com/tendermint/tendermint/tree/master/libs/db"
+	tmtypes "github.com/tendermint/tendermint/tree/master/types"
 )
 
 const (
