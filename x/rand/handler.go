1f942985f05d02f85274811fe1c311d2450ac448
DecentRandom <misskiwi@decentrandom.com>
2019-03-06 17:02:35 +0900

    Update handler.go

--- x/rand/handler.go
+++ x/rand/handler.go
@@ -1,1 +1,27 @@
 package rand
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// NewHandler returns a handler for "rand" type messages.
+func NewHandler(keeper Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
+		switch msg := msg.(type) {
+
+		/*
+			to-do
+		*/
+
+		default:
+			errMsg := fmt.Sprintf("Unrecognized rand Msg type: %v", msg.Type())
+			return sdk.ErrUnknownRequest(errMsg).Result()
+		}
+	}
+}
+
+/*
+to-do
+*/
