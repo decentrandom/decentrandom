d05b6c1442b8a915a31a1bc166e44745967bc18a
DecentRandom <misskiwi@decentrandom.com>
2019-03-07 02:49:33 +0900

    Create tx.go

---
+++ x/rand/client/cli/tx.go
@@ -0,0 +1,17 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/utils"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/decentrandom/decentrandom/x/rand"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtxb "github.com/cosmos/cosmos-sdk/x/auth/client/txbuilder"
+)
+
+/*
+to-do
+*/
