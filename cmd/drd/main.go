c5cd89d37f9004239a485be617c6d963108db57c
DecentRandom <misskiwi@decentrandom.com>
2019-03-07 16:16:27 +0900

    Create main.go

---
+++ cmd/drd/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/libs/cli"
+	"github.com/tendermint/tendermint/libs/common"
+	"github.com/tendermint/tendermint/libs/log"
+
+	gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	app "github.com/decentrandom/decentrandom"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cfg "github.com/tendermint/tendermint/config"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+var DefaultNodeHome = os.ExpandEnv("$HOME/.drd")
+
+const (
+	flagOverwrite = "overwrite"
+)
+
+func main() {
+	cobra.EnableCommandSorting = false
+
+	cdc := app.MakeCodec()
+	ctx := server.NewDefaultContext()
+
+	rootCmd := &cobra.Command {
+		Use:				"drd",
+		Short:				"decentrandom App Daemon (server)",
+		PersistentPreRunE:	server.PersistentPreRunEFn(ctx),
+	}
+
+	rootCmd.AddCommand(InitCmd(ctx, cdc))
+	rootCmd.AddCommand(AddGenesisAccountCmd(ctx, cdc))
+
+	server.AddCommands(ctx, cdc, rootCmd, newApp, appExporter())
+
+	// prepare and add flags
+	executor := cli.PrepareBaseCmd(rootCmd, "DR", DefaultNodeHome)
+	err := executor.Execute()
+	if err != nil {
+		// handle with #870
+		panic(err)
+	}
+}
+
+func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application {
+	return app.DecentRandomApp(logger, db)
+}
+
+func appExporter() server.AppExporter {
+	return func(logger log.logger, db dbm.DB, _ io.Writer, _ int64, _ bool, _ []string) ()
+		json.RawMessage, []tmtypes.GenesisValidator, error) {
+			dapp := app.NewDecentRandomApp(logger, db)
+			return dapp.ExportAppStateAndValidators()
+		}
+	}
+}
+
+func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command {
+		Use:		"init",
+		Short:		"Initialize genesis config, priv-validator file, and p2p-node file",
+		Args:		cobra.NoArgs,
+		RunE:		func(_ *cobra.Command, _ []string) error {
+			config := ctx.Config
+			config.SetRoot(viper.GetString(clie.HomeFlag))
+
+			chainID := viper.GetString(client.FlagChainID)
+			if chainID == "" {
+				chainID = fmt.Sprintf("test-chain-%v", common.RandStr(6))
+			}
+
+			_, pk, err := gaiaInit.InitializeNodeValidatorFiles(config)
+			if err != nil {
+				return err
+			}
+
+			var appState json.RawMessage
+			genFile := config.GenesisFile()
+
+			if !viper.GetBool(flagOverwrite) && common.FileExists(genFile) {
+				reutrn fmt.Errof("genesis.json file already exists: $v", genFile)
+			}
+		}
+	}
+}
