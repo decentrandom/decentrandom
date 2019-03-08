b0730cc0d9926c286b5e187069eecd184c949f2d
DecentRandom <misskiwi@decentrandom.com>
2019-03-09 04:13:00 +0900

    Update types.go

--- x/rand/types.go
+++ x/rand/types.go
@@ -1,6 +1,8 @@
 package rand
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -9,15 +11,22 @@ import (
 //var DefaultRoundDifficulty = 1
 
 type Round struct {
-	Difficulty  int16          `json:"difficulty"`
-	Owner       sdk.AccAddress `json:"owner"`
-	Nonce       int16          `json:"nonce"`
-	NonceHash   string         `json:"nonce_hash"`
-	Targets     []string       `json:"targets"`
-	RunTime     time.Time      `json:"time"`
-	SeedHeights []int64        `json:"seed_heights"`
+	Difficulty    int16          `json:"difficulty"`
+	Owner         sdk.AccAddress `json:"owner"`
+	Nonce         int16          `json:"nonce"`
+	NonceHash     string         `json:"nonce_hash"`
+	Targets       []string       `json:"targets"`
+	ScheduledTime time.Time      `json:"scheduled_time"`
+	SeedHeights   []int64        `json:"seed_heights"`
 }
 
 func NewRound() Round {
 	return Round{}
+}
+
+func (r Round) String() string {
+	return strings.TrimSpace(fmt.Sprintf(`Owner: %s
+Difficulty: %d
+Nonce: %d
+NonceHash: %s`))
 }
