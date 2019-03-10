848121b0ca1a7daa36fbae2277d40e38cef2a1a0
DecentRandom <misskiwi@decentrandom.com>
2019-03-10 17:16:46 +0900

    Update msgs.go

--- x/rand/msgs.go
+++ x/rand/msgs.go
@@ -2,10 +2,52 @@ package rand
 
 import (
 	"encoding/json"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-/*
-to-do
-*/
+type MsgNewRound struct {
+	Id            string
+	Difficulty    int16
+	Owner         sdk.AccAddress
+	NonceHash     string
+	Targets       []string
+	ScheduledTime time.Time
+}
+
+func NewMsgNewRound(id string, difficulty int16, owner sdk.AccAddress, nonceHash string, targets []string, scheduledTime time.Time) MsgNewRound {
+	return MsgNewRound{
+		Id:            id,
+		Difficulty:    difficulty,
+		Owner:         owner,
+		NonceHash:     nonceHash,
+		Targets:       targets,
+		ScheduledTime: scheduledTime,
+	}
+}
+
+func (msg MsgNewRound) Route() string {
+	return "rand"
+}
+
+func (msg MsgNewRound) Type() string {
+	return "new_round"
+}
+
+// Validate
+func (msg MsgNewRound) ValidateBasic() sdk.Error {
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
+	}
+
+	if len(msg.Id) == 0 || len(msg.NonceHash) == 0 {
+		return sdk.ErrUnknownRequest("Id and/or NonceHash cannot be empty.")
+	}
+
+	if msg.Difficulty < 1 {
+		return sdk.ErrUnknownRequest("Difficulty must greater than zero(0).")
+	}
+
+	return nil
+}
