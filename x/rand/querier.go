94f9018e53a9ac20f1b0b77f570323d2e4fc88bb
DecentRandom <misskiwi@decentrandom.com>
2019-03-11 12:26:49 +0900

    Update querier.go

--- x/rand/querier.go
+++ x/rand/querier.go
@@ -1,6 +1,9 @@
 package rand
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -8,11 +11,22 @@ import (
 )
 
 const (
-/*
-	to-do
-*/
+	QueryIds = "ids"
 )
 
-/*
-to-do
-*/
+func NewQuerier(keeper Keeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
+		switch path[0] {
+		case QueryIds:
+			return queryIds(ctx, req, keeper)
+
+		default:
+			return nil, sdk.ErrUnknownRequest("unknown rand query endpoint.")
+		}
+	}
+}
+
+func queryIds(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+
+	// to-do
+}
