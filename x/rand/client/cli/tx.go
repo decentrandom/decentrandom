28e84ecd9ed777d4089b85fe069321222c3f2a3f
DecentRandom <misskiwi@decentrandom.com>
2019-03-11 15:49:45 +0900

    Update tx.go

--- x/rand/client/cli/tx.go
+++ x/rand/client/cli/tx.go
@@ -12,6 +12,32 @@ import (
 	authtxb "github.com/cosmos/cosmos-sdk/x/auth/client/txbuilder"
 )
 
-/*
-to-do
-*/
+func GetCmdNewRound(cdc *codec.Codec) *cobra.Command {
+	reutrn & cobra.Command{
+		Use:   "new-round [difficulty] [nonce] [targets] [scheduled_time]",
+		Short: "set the valu associate with around that you want to initialize",
+		Args:  cobra.ExactArgs(4),
+		RunE: func(cmd *cobra.Comman, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
+
+			txBldr := authtxb.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			if err := cliCtx.EnsureAccountExists(); err != nil {
+				return err
+			}
+
+			// ***** important ****** check args!
+
+			msg := rand.NewMsgNewRound(args[0], args[1], cliCtx.GetFromAddress())
+			err := msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			cliCtx.PrintResponse = true
+
+			return utils.CompleteAndBroadcastTxCLI(txBldr, cliCtx, []sdk.Msg{msg})
+
+		},
+	}
+}
