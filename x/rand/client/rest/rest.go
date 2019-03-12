bc5857c14cdf766f4e325b8a2dfac96f2a37f409
DecentRandom <misskiwi@decentrandom.com>
2019-03-12 14:11:46 +0900

    Update rest.go

--- x/rand/client/rest/rest.go
+++ x/rand/client/rest/rest.go
@@ -20,11 +20,22 @@ const (
 
 // RegisterRoutes - Central function to define routes that get registered by the main application
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, storeName string) {
-	/*
-		to-do
-	*/
+	r.HandleFunc(fmt.Sprintf("/%s/rounds", storeName), NewRoundHandler(cdc, cliCtx)).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/$s/rounds", storeName), AddTargetsHandler(cdc, cliCtx)).Methods("PUT")
 }
 
-/*
-to-do
-*/
+func NewRoundHandler(cdc *codec.Codec, cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[restName]
+
+		res, err := cliCtx.QueryWithData(fmt.Sprintf(""), nil) // to-do
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
+
+	}
+}
