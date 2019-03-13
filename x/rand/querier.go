a5bc13442435b4a24320a379c87bb6db4c058d2c
DecentRandom <misskiwi@decentrandom.com>
2019-03-14 03:57:33 +0900

    Update querier.go

--- x/rand/querier.go
+++ x/rand/querier.go
@@ -13,6 +13,7 @@ const (
 	QueryIds = "ids"
 )
 
+// NewQuerier -
 func NewQuerier(keeper Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
 		switch path[0] {
@@ -27,9 +28,9 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 
 func queryIds(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
 
-	var idsList QueryResIds
+	var idsList QueryResIDs
 
-	iterator := keeper.GetIdsIterator(ctx)
+	iterator := keeper.GetIDsIterator(ctx)
 
 	for ; iterator.Valid(); iterator.Next() {
 		id := string(iterator.Key())
@@ -44,8 +45,9 @@ func queryIds(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) (res []byte
 	return bz, nil
 }
 
-type QueryResIds []string
+// QueryResIDs -
+type QueryResIDs []string
 
-func (n QueryResIds) String() string {
+func (n QueryResIDs) String() string {
 	return strings.Join(n[:], "\n")
 }
