9819ee4b040c7cd93bd8fec2e5b231a51b3495f0
DecentRandom <misskiwi@decentrandom.com>
2019-03-14 15:04:38 +0900

    Update querier.go

--- x/rand/querier.go
+++ x/rand/querier.go
@@ -16,6 +16,7 @@ const (
 
 // NewQuerier -
 func NewQuerier(keeper Keeper) sdk.Querier {
+
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
 		switch path[0] {
 		case QueryIDs:
