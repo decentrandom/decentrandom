08f845e0bb1c563868270bae7dd3ccd2b047012a
DecentRandom <misskiwi@decentrandom.com>
2019-03-20 04:30:48 +0900

    Update types.go

--- x/rand/types.go
+++ x/rand/types.go
@@ -1,8 +1,6 @@
 package rand
 
 import (
-	"fmt"
-	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -19,16 +17,4 @@ type Round struct {
 	Targets       []string       `json:"targets"`
 	ScheduledTime time.Time      `json:"scheduled_time"`
 	SeedHeights   []int64        `json:"seed_heights"`
-}
-
-func (r Round) String() string {
-	timeString := r.ScheduledTime.Local()
-	return strings.TrimSpace(fmt.Sprintf(`Owner: %s
-Difficulty: %d
-Nonce: %d
-NonceHash: %s
-Targets: %v
-ScheduledTime: %s
-SeedHeights: %v
-`, r.Owner, r.Difficulty, r.Nonce, r.NonceHash, r.Targets, timeString.Format("2006-01-02 15:04:05 +0900"), r.SeedHeights))
 }
