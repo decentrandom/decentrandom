f85b4257e93f409225a6ad17087a8ca90bb891df
DecentRandom <misskiwi@decentrandom.com>
2019-03-20 04:28:08 +0900

    Update querier.go

--- x/rand/querier.go
+++ x/rand/querier.go
@@ -1,1 +1,43 @@
 package rand
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// query endpoints
+const (
+	queryRound = "round"
+)
+
+// NewQuerier -
+func NewQuerier(keeper Keeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
+
+		switch path[0] {
+		case queryRound:
+			return queryRound(ctx, path[1:], req, keeper)
+		default:
+			return nil, sdk.ErrUnknownRequest("unknown query endpoint")
+		}
+	}
+}
+
+// queryRound
+func queryRound(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+	id := path[0]
+
+	round := keeper.GetRound(ctx, id)
+
+	bz, err2 := codec.MarshalJSONIndent(keeper.cdc, round)
+	if err2 != nil {
+		panice("could not marshal result to JSON")
+	}
+
+	return btz, nil
+}
