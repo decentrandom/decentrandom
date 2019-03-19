c8d81b765f72b4adfc112491dc113ad9c5c28fba
DecentRandom <misskiwi@decentrandom.com>
2019-03-19 14:13:06 +0900

    Update msgs.go

--- x/rand/msgs.go
+++ x/rand/msgs.go
@@ -7,6 +7,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+/*
+NewRound
+*/
+
 // MsgNewRound is a struct format for a message
 type MsgNewRound struct {
 	ID            string
@@ -74,6 +78,67 @@ func (msg MsgNewRound) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Owner}
 }
 
+/*
+DeployNonce
+*/
+
+// MsgDeployNonce is a struct format for a message
+type MsgDeployNonce struct {
+	ID    string
+	Owner sdk.AccAddress
+	Nonce int16
+}
+
+// NewMsgDeployNonce - Nonce must be 0, SeedHeights must be nil
+func NewMsgDeployNonce(id string, difficulty int16, owner sdk.AccAddress, nonce int16) NewMsgDeployNonce {
+	return MsgDeployNonce{
+		ID:    id,
+		Owner: owner,
+		Nonce: nonce,
+	}
+}
+
+// Route -
+func (msg MsgDeployNonce) Route() string {
+	return "rand"
+}
+
+// Type -
+func (msg MsgDeployNonce) Type() string {
+	return "deploy_round"
+}
+
+// ValidateBasic -
+func (msg MsgDeployNonce) ValidateBasic() sdk.Error {
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
+	}
+
+	if len(msg.ID) == 0 {
+		return sdk.ErrUnknownRequest("Id and/or NonceHash cannot be empty.")
+	}
+
+	return nil
+}
+
+// GetSignBytes -
+func (msg MsgDeployNonce) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return sdk.MustSortJSON(b)
+}
+
+// GetSigners -
+func (msg MsgDeployNonce) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Owner}
+}
+
+/*
+AddTargets
+*/
+
 // MsgAddTargets -
 type MsgAddTargets struct {
 	ID      string
@@ -81,7 +146,7 @@ type MsgAddTargets struct {
 	Targets []string
 }
 
-//NewMsgAddTargets -
+// NewMsgAddTargets -
 func NewMsgAddTargets(id string, owner sdk.AccAddress, targets []string) MsgAddTargets {
 	return MsgAddTargets{
 		ID:      id,
@@ -127,5 +192,63 @@ func (msg MsgAddTargets) GetSignBytes() []byte {
 
 // GetSigners -
 func (msg MsgAddTargets) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Owner}
+}
+
+// MsgRemoveTargets
+
+// MsgRemoveTargets -
+type MsgRemoveTargets struct {
+	ID      string
+	Owner   sdk.AccAddress
+	Targets []string
+}
+
+// NewMsgRemoveTargets -
+func NewMsgRemoveTargets(id string, owner sdk.AccAddress, targets []string) MsgRemoveTargets {
+	return MsgRemoveTargets{
+		ID:      id,
+		Owner:   owner,
+		Targets: targets,
+	}
+}
+
+// Route -
+func (msg MsgRemoveTargets) Route() string {
+	return "rand"
+}
+
+// Type -
+func (msg MsgRemoveTargets) Type() string {
+	return "remove_targets"
+}
+
+// ValidateBasic -
+func (msg MsgRemoveTargets) ValidateBasic() sdk.Error {
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
+	}
+	if len(msg.ID) == 0 {
+		return sdk.ErrUnknownRequest("Round ID cannot be empty.")
+	}
+
+	if msg.Targets == nil {
+		return sdk.ErrUnknownRequest("Targets cannot be empty.")
+	}
+
+	return nil
+}
+
+// GetSignBytes -
+func (msg MsgRemoveTargets) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return sdk.MustSortJSON(b)
+}
+
+// GetSigners -
+func (msg MsgRemoveTargets) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Owner}
 }
