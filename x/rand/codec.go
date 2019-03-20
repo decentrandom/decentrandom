9018091b4ea8d517fa197486658e8e4f2c0cea73
DecentRandom <misskiwi@decentrandom.com>
2019-03-20 13:23:02 +0900

    Update codec.go

--- x/rand/codec.go
+++ x/rand/codec.go
@@ -8,5 +8,7 @@ import (
 func RegisterCodec(cdc *codec.Codec) {
 
 	cdc.RegisterConcrete(MsgNewRound{}, "rand/NewRound", nil)
+	cdc.RegisterConcrete(MsgDeployNonce{}, "rand/DeployNonce", nil)
 	cdc.RegisterConcrete(MsgAddTargets{}, "rand/AddTargets", nil)
+	cdc.RegisterConcrete(MsgRemoveTargets{}, "rand/RemoveTargets", nil)
 }
