d1abda76528dd0be9bff671fd678bbe92ac9f950
DecentRandom <misskiwi@decentrandom.com>
2019-03-20 13:23:05 +0900

    Update query.go

--- x/rand/client/cli/query.go
+++ x/rand/client/cli/query.go
@@ -1,1 +1,33 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/decentrandom/decentrandom/x/rand"
+	"github.com/spf13/cobra"
+)
+
+// GetCmdRound -
+func GetCmdRound(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "round [id]",
+		Short: "Query round info of ID",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []sgring) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			id := args[0]
+
+			res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/round/%s", queryRoute, id), nil)
+			if err != nil {
+				fmt.Printf("could not get round - %s \n", string(id))
+				return nil
+			}
+
+			var out rand.Round
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(out)
+		},
+	}
+}
