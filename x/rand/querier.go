4cedac1ec039875fb92bf542d8e0df5925b02859
DecentRandom <misskiwi@decentrandom.com>
2019-03-22 13:24:17 +0900

    Update querier.go

--- x/rand/querier.go
+++ x/rand/querier.go
@@ -12,7 +12,7 @@ import (
 
 // query endpoints
 const (
-	QueryRound = "round"
+	QueryRoundInfo = "round_info"
 )
 
 // NewQuerier -
@@ -20,8 +20,8 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
 
 		switch path[0] {
-		case QueryRound:
-			return queryRound(ctx, path[1:], req, keeper)
+		case QueryRoundInfo:
+			return queryRoundInfo(ctx, path[1:], req, keeper)
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown query endpoint")
 		}
@@ -29,7 +29,7 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 }
 
 // queryRound
-func queryRound(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+func queryRoundInfo(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
 	id := path[0]
 
 	round := keeper.GetRound(ctx, id)
