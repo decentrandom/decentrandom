d16c957d7cdf2b7e682714e04cb442bf09b12d37
DecentRandom <misskiwi@decentrandom.com>
2019-03-27 15:37:35 +0900

    Update keeper.go

--- x/rand/keeper.go
+++ x/rand/keeper.go
@@ -7,16 +7,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// Keeper -
+// Keeper - struct 선언
 type Keeper struct {
 	coinKeeper bank.Keeper
 
 	storeKey sdk.StoreKey
 
-	cdc *codec.Codec // The wire codec for binary encoding/decoding.
+	cdc *codec.Codec
 }
 
-// NewKeeper -
+// NewKeeper - keeper 생성
 func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
 	return Keeper{
 		coinKeeper: coinKeeper,
@@ -25,7 +25,7 @@ func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) 
 	}
 }
 
-// SetRound is setter method for the round
+// SetRound - 라운드 setter
 func (k Keeper) SetRound(ctx sdk.Context, id string, round Round) {
 	if len(id) == 0 || round.Owner.Empty() {
 		return
@@ -35,7 +35,7 @@ func (k Keeper) SetRound(ctx sdk.Context, id string, round Round) {
 	store.Set([]byte(id), k.cdc.MustMarshalBinaryBare(round))
 }
 
-// GetRound is getter method for the round
+// GetRound - 라운드 getter
 func (k Keeper) GetRound(ctx sdk.Context, id string) Round {
 	store := ctx.KVStore(k.storeKey)
 	var round Round
@@ -49,31 +49,31 @@ func (k Keeper) GetRound(ctx sdk.Context, id string) Round {
 	return round
 }
 
-// GetOwner -
+// GetOwner - 라운드 소유자 getter
 func (k Keeper) GetOwner(ctx sdk.Context, id string) sdk.AccAddress {
 	return k.GetRound(ctx, id).Owner
 }
 
-// GetTargets - 설정된 타겟 받아오기
+// GetTargets - 라운드 모집단 getter
 func (k Keeper) GetTargets(ctx sdk.Context, id string) []string {
 	return k.GetRound(ctx, id).Targets
 }
 
-// SetTargets -
+// SetTargets - 라운드 모집단 setter
 func (k Keeper) SetTargets(ctx sdk.Context, id string, targets []string) {
 	round := k.GetRound(ctx, id)
 	round.Targets = targets
 	k.SetRound(ctx, id, round)
 }
 
-// SetNonce -
+// SetNonce - 라운드 Nonce setter
 func (k Keeper) SetNonce(ctx sdk.Context, id string, nonce int16) {
 	round := k.GetRound(ctx, id)
 	round.Nonce = nonce
 	k.SetRound(ctx, id, round)
 }
 
-// GetIDsIterator -
+// GetIDsIterator - 전체 라운드 ID getter
 func (k Keeper) GetIDsIterator(ctx sdk.Context) sdk.Iterator {
 	store := ctx.KVStore(k.storeKey)
 	return sdk.KVStorePrefixIterator(store, []byte{})
