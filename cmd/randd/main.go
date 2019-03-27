cd094ed439ec5b639087c31b9fb71bad1e5da8b9
DecentRandom <misskiwi@decentrandom.com>
2019-03-27 12:06:43 +0900

    Update main.go

--- cmd/randd/main.go
+++ cmd/randd/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	rootCmd := &cobra.Command{
 		Use:               "randd",
-		Short:             "DecentRandom App Daemon (server)",
+		Short:             "DecentRandom 앱 데몬 (서버)",
 		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
 	}
 
@@ -80,7 +80,7 @@ func appExporter() server.AppExporter {
 func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "init",
-		Short: "Initialize genesis config, priv-validator file, and p2p-node file",
+		Short: "제네시스 설정, priv-validator 파일, P2P 노드 파일을 초기화 합니다.",
 		Args:  cobra.NoArgs,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			config := ctx.Config
@@ -124,14 +124,14 @@ func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 
 			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
 
-			fmt.Printf("Initialized drd configuration and bootstrapping files in %s...\n", viper.GetString(cli.HomeFlag))
+			fmt.Printf("%s - 설정 및 부트스트랩 파일을 초기화하였습니다....\n", viper.GetString(cli.HomeFlag))
 			return nil
 		},
 	}
 
-	cmd.Flags().String(cli.HomeFlag, DefaultNodeHome, "node's home directory")
-	cmd.Flags().String(client.FlagChainID, "", "genesis file chain-id, if left blank will be randomly created")
-	cmd.Flags().BoolP(flagOverwrite, "o", false, "overwrite the genesis.json file")
+	cmd.Flags().String(cli.HomeFlag, DefaultNodeHome, "노드의 홈 디렉토리")
+	cmd.Flags().String(client.FlagChainID, "", "제네시스 파일 체인 ID 입니다. 공백으로 남겨두면 임의로 생성합니다.")
+	cmd.Flags().BoolP(flagOverwrite, "o", false, "genesis.json 파일을 덮어씁니다.")
 
 	return cmd
 }
@@ -140,11 +140,11 @@ func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 func AddGenesisAccountCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "add-genesis-account [address] [coins[,coins]]",
-		Short: "Adds an account to the genesis file",
+		Short: "제네시스 파일에 계정을 추가합니다.",
 		Args:  cobra.ExactArgs(2),
 		Long: strings.TrimSpace(`
-		Adds accounts to the genesis file so that you can start a chain with coins in the CLI:
-		$ drd add-genesis-account cosmos1tse7r2fadvlrrgau3pa0ss7cqh55wrv6y9alwh 1000STAKE,1000mycoin
+		제네시스 파일을 추가하여 CLI를 통해 코인과 함께 체인을 구동할 수 있도록 합니다.:
+		$ randd add-genesis-account cosmos1tse7r2fadvlrrgau3pa0ss7cqh55wrv6y9alwh 1000STAKE,1000mycoin
 		`),
 		RunE: func(_ *cobra.Command, args []string) error {
 			addr, err := sdk.AccAddressFromBech32(args[0])
@@ -161,7 +161,7 @@ func AddGenesisAccountCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command 
 			config := ctx.Config
 			genFile := config.GenesisFile()
 			if !common.FileExists(genFile) {
-				return fmt.Errorf("%s does not exist, run `gaiad init` first", genFile)
+				return fmt.Errorf("%s이 존재하지 않습니다. `gaiad init` 를 먼저 실행하시기 바랍니다.", genFile)
 			}
 			genContents, err := ioutil.ReadFile(genFile)
 			if err != nil {
@@ -178,7 +178,7 @@ func AddGenesisAccountCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command 
 
 			for _, stateAcc := range appState.Accounts {
 				if stateAcc.Address.Equals(addr) {
-					return fmt.Errorf("the application state already contains account %v", addr)
+					return fmt.Errorf("이미 %v 계정에 대한 정보를 가지고 있습니다.", addr)
 				}
 			}
 
