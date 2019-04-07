d2d4df75d99e43cbb7b44d94d5d019c9ebce4a43
DecentRandom <misskiwi@decentrandom.com>
2019-04-08 02:15:32 +0900

    Update main.go

--- cmd/randd/main.go
+++ cmd/randd/main.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	//"fmt"
 	"io"
 	// "io/ioutil"
 	//"os"
-	"path/filepath"
+	//"path/filepath"
 	// "strings"
 
-	"github.com/cosmos/cosmos-sdk/client"
+	//"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
-	"github.com/cosmos/cosmos-sdk/x/auth"
-	"github.com/cosmos/cosmos-sdk/x/bank"
+	//"github.com/cosmos/cosmos-sdk/x/auth"
+	//"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/decentrandom/decentrandom/types/util"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	//"github.com/spf13/viper"
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/cli"
-	"github.com/tendermint/tendermint/libs/common"
+	//"github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/log"
 
-	gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
+	//gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	app "github.com/decentrandom/decentrandom"
 	randInit "github.com/decentrandom/decentrandom/cmd/init"
 	abci "github.com/tendermint/tendermint/abci/types"
-	cfg "github.com/tendermint/tendermint/config"
+	//cfg "github.com/tendermint/tendermint/config"
 	dbm "github.com/tendermint/tendermint/libs/db"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
@@ -86,66 +86,6 @@ func exportAppStateAndTMValidators(
 	}
 	rApp := app.NewRandApp(logger, db)
 	return rApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
-}
-
-// InitCmd - init 명령어
-func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "init",
-		Short: "제네시스 설정, priv-validator 파일, P2P 노드 파일을 초기화 합니다.",
-		Args:  cobra.NoArgs,
-		RunE: func(_ *cobra.Command, _ []string) error {
-			config := ctx.Config
-			config.SetRoot(viper.GetString(cli.HomeFlag))
-
-			chainID := viper.GetString(client.FlagChainID)
-			if chainID == "" {
-				chainID = fmt.Sprintf("test-chain-%v", common.RandStr(6))
-			}
-
-			_, pk, err := gaiaInit.InitializeNodeValidatorFiles(config)
-			if err != nil {
-				return err
-			}
-
-			var appState json.RawMessage
-			genFile := config.GenesisFile()
-
-			if !viper.GetBool(flagOverwrite) && common.FileExists(genFile) {
-				return fmt.Errorf("genesis.json file already exists: %v", genFile)
-			}
-
-			genesis := app.GenesisState{
-				AuthData: auth.DefaultGenesisState(),
-				BankData: bank.DefaultGenesisState(),
-			}
-
-			appState, err = codec.MarshalJSONIndent(cdc, genesis)
-			if err != nil {
-				return err
-			}
-
-			_, _, validator, err := SimpleAppGenTx(cdc, pk)
-			if err != nil {
-				return err
-			}
-
-			if err = gaiaInit.ExportGenesisFile(genFile, chainID, []tmtypes.GenesisValidator{validator}, appState); err != nil {
-				return err
-			}
-
-			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
-
-			fmt.Printf("%s - 설정 및 부트스트랩 파일을 초기화하였습니다....\n", viper.GetString(cli.HomeFlag))
-			return nil
-		},
-	}
-
-	cmd.Flags().String(cli.HomeFlag, app.DefaultNodeHome, "노드의 홈 디렉토리")
-	cmd.Flags().String(client.FlagChainID, "", "제네시스 파일 체인 ID 입니다. 공백으로 남겨두면 임의로 생성합니다.")
-	cmd.Flags().BoolP(flagOverwrite, "o", false, "genesis.json 파일을 덮어씁니다.")
-
-	return cmd
 }
 
 // SimpleAppGenTx -
