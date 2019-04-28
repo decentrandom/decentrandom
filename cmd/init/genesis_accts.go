8ded341bacf134a6f62cfc7d84b63aa34d056a44
DecentRandom <misskiwi@decentrandom.com>
2019-04-29 02:25:15 +0900

    Update genesis_accts.go

--- cmd/init/genesis_accts.go
+++ cmd/init/genesis_accts.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/client/keys"
-	//"github.com/cosmos/cosmos-sdk/cmd/gaia/app"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/decentrandom/decentrandom/app"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tendermint/tendermint/libs/cli"
 	"github.com/tendermint/tendermint/libs/common"
-	//tmtypes "github.com/tendermint/tendermint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // AddGenesisAccountCmd -
