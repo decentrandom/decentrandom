f0f03819573cfe75b4ad8d64dd8cdf170ca2136f
DecentRandom <misskiwi@decentrandom.com>
2019-04-29 11:48:20 +0900

    Update utils.go

--- server/utils.go
+++ server/utils.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"net"
 	"os"
 	"os/signal"
@@ -9,10 +10,14 @@ import (
 	"syscall"
 	"time"
 
-	"errors"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/server/config"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	tcmd "github.com/tendermint/tendermint/cmd/tendermint/commands"
 	cfg "github.com/tendermint/tendermint/config"
@@ -20,19 +25,9 @@ import (
 	tmflags "github.com/tendermint/tendermint/libs/cli/flags"
 	"github.com/tendermint/tendermint/libs/log"
 	pvm "github.com/tendermint/tendermint/privval"
-
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/server"
-	"github.com/cosmos/cosmos-sdk/server/config"
-	"github.com/cosmos/cosmos-sdk/version"
 )
 
-//___________________________________________________________________________________
-
-// PersistentPreRunEFn returns a PersistentPreRunE function for cobra
-// that initailizes the passed in context with a properly configured
-// logger and config object.
+// PersistentPreRunEFn -
 func PersistentPreRunEFn(context *server.Context) func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) error {
 		if cmd.Name() == version.VersionCmd.Name() {
@@ -61,35 +56,30 @@ func PersistentPreRunEFn(context *server.Context) func(*cobra.Command, []string)
 	}
 }
 
-// If a new config is created, change some of the default tendermint settings
+// interceptLoadConfig -
 func interceptLoadConfig() (conf *cfg.Config, err error) {
 	tmpConf := cfg.DefaultConfig()
 	err = viper.Unmarshal(tmpConf)
 	if err != nil {
-		// TODO: Handle with #870
 		panic(err)
 	}
 	rootDir := tmpConf.RootDir
 	configFilePath := filepath.Join(rootDir, "config/config.toml")
-	// Intercept only if the file doesn't already exist
 
 	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		// the following parse config is needed to create directories
-		conf, _ = tcmd.ParseConfig() // NOTE: ParseConfig() creates dir/files as necessary.
+		conf, _ = tcmd.ParseConfig()
 		conf.ProfListenAddress = "localhost:6060"
 		conf.P2P.RecvRate = 5120000
 		conf.P2P.SendRate = 5120000
 		conf.TxIndex.IndexAllTags = true
 		conf.Consensus.TimeoutCommit = 5 * time.Second
 		cfg.WriteConfigFile(configFilePath, conf)
-		// Fall through, just so that its parsed into memory.
 	}
 
 	if conf == nil {
-		conf, err = tcmd.ParseConfig() // NOTE: ParseConfig() creates dir/files as necessary.
+		conf, err = tcmd.ParseConfig()
 	}
 
-	// create a default randd config file if it does not exist
 	randConfigFilePath := filepath.Join(rootDir, "config/randd.toml")
 	if _, err := os.Stat(randConfigFilePath); os.IsNotExist(err) {
 		randConf, _ := config.ParseConfig()
@@ -102,7 +92,7 @@ func interceptLoadConfig() (conf *cfg.Config, err error) {
 	return
 }
 
-// validate the config with the sdk's requirements.
+// validateConfig -
 func validateConfig(conf *cfg.Config) error {
 	if !conf.Consensus.CreateEmptyBlocks {
 		return errors.New("config option CreateEmptyBlocks = false is currently unsupported")
@@ -110,7 +100,7 @@ func validateConfig(conf *cfg.Config) error {
 	return nil
 }
 
-// add server commands
+// AddCommands -
 func AddCommands(
 	ctx *server.Context, cdc *codec.Codec,
 	rootCmd *cobra.Command,
@@ -143,11 +133,7 @@ func AddCommands(
 
 //___________________________________________________________________________________
 
-// InsertKeyJSON inserts a new JSON field/key with a given value to an existing
-// JSON message. An error is returned if any serialization operation fails.
-//
-// NOTE: The ordering of the keys returned as the resulting JSON message is
-// non-deterministic, so the client should not rely on key ordering.
+// InsertKeyJSON -
 func InsertKeyJSON(cdc *codec.Codec, baseJSON []byte, key string, value json.RawMessage) ([]byte, error) {
 	var jsonMap map[string]json.RawMessage
 
@@ -161,8 +147,7 @@ func InsertKeyJSON(cdc *codec.Codec, baseJSON []byte, key string, value json.Raw
 	return json.RawMessage(bz), err
 }
 
-// https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
-// TODO there must be a better way to get external IP
+// ExternalIP -
 func ExternalIP() (string, error) {
 	ifaces, err := net.Interfaces()
 	if err != nil {
@@ -191,7 +176,7 @@ func ExternalIP() (string, error) {
 	return "", errors.New("are you connected to the network?")
 }
 
-// TrapSignal traps SIGINT and SIGTERM and terminates the server correctly.
+// TrapSignal -
 func TrapSignal(cleanupFunc func()) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -208,8 +193,7 @@ func TrapSignal(cleanupFunc func()) {
 	}()
 }
 
-// UpgradeOldPrivValFile converts old priv_validator.json file (prior to Tendermint 0.28)
-// to the new priv_validator_key.json and priv_validator_state.json files.
+// UpgradeOldPrivValFile -
 func UpgradeOldPrivValFile(config *cfg.Config) {
 	if _, err := os.Stat(config.OldPrivValidatorFile()); !os.IsNotExist(err) {
 		if oldFilePV, err := pvm.LoadOldFilePV(config.OldPrivValidatorFile()); err == nil {
@@ -218,16 +202,18 @@ func UpgradeOldPrivValFile(config *cfg.Config) {
 	}
 }
 
+// skipInterface -
 func skipInterface(iface net.Interface) bool {
 	if iface.Flags&net.FlagUp == 0 {
-		return true // interface down
+		return true
 	}
 	if iface.Flags&net.FlagLoopback != 0 {
-		return true // loopback interface
+		return true
 	}
 	return false
 }
 
+// addrToIP -
 func addrToIP(addr net.Addr) net.IP {
 	var ip net.IP
 	switch v := addr.(type) {
