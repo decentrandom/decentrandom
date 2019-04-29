9e10fdc74b2285080b5579557010011550c2fba1
DecentRandom <misskiwi@decentrandom.com>
2019-04-29 11:58:42 +0900

    Update codec.go

--- types/codec.go
+++ types/codec.go
@@ -2,7 +2,7 @@ package types
 
 import "github.com/cosmos/cosmos-sdk/codec"
 
-// RegisterCodec register type codec
+// RegisterCodec -
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(&Schedule{}, "core/Schedule", nil)
 	cdc.RegisterConcrete(&VestingSchedule{}, "core/VestingSchedule", nil)
