d87aefa00618df42a24abbf0024afaf554486fb6
DecentRandom <misskiwi@decentrandom.com>
2019-05-09 15:38:12 +0900

    Update prefix.go

--- types/util/prefix.go
+++ types/util/prefix.go
@@ -1,16 +1,16 @@
 package util
 
 const (
-	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	// Bech32PrefixAccAddr - 기본적인 계정 주소
 	Bech32PrefixAccAddr = "rand"
-	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	// Bech32PrefixAccPub - 계정의 공개 키
 	Bech32PrefixAccPub = "randpub"
-	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	// Bech32PrefixValAddr - 검증인 오퍼레이터 주소
 	Bech32PrefixValAddr = "randvaloper"
-	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	// Bech32PrefixValPub - 검증인 오퍼레이터의 공개 키
 	Bech32PrefixValPub = "randvaloperpub"
-	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	// Bech32PrefixConsAddr - 합의 노드 주소
 	Bech32PrefixConsAddr = "randvalcons"
-	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	// Bech32PrefixConsPub - 합의 노드의 공개 키
 	Bech32PrefixConsPub = "randvalconspub"
 )
