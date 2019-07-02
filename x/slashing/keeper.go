c64a8bfc1e054fd77164544f0d5ffd8a3c7cfbaf
DecentRandom <misskiwi@decentrandom.com>
2019-07-02 13:27:21 +0900

    Update keeper.go

--- x/slashing/keeper.go
+++ x/slashing/keeper.go
@@ -3,11 +3,14 @@ package slashing
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 )
 
+// StoreKey -
 const StoreKey = "slashing"
 
+// Keeper -
 type Keeper struct {
 	storeKey   sdk.StoreKey
 	cdc        *codec.Codec
