c0bfe36740864313a5d824dc9d48257c7fcda312
DecentRandom <misskiwi@decentrandom.com>
2019-07-02 16:56:28 +0900

    Create module_client.go

---
+++ x/slashing/client/module_client.go
@@ -0,0 +1,9 @@
+package client
+
+import (
+	"github.com/spf13/cobra"
+	amino "github.com/tendermint/go-amino"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+)
