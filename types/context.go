eae474879de0ca24d1b78e54b70920291685d02a
decentrandom <misskiwi@decentrandom.com>
2019-07-04 14:38:46 +0900

    Update context.go

--- types/context.go
+++ types/context.go
@@ -7,6 +7,20 @@ import (
 	"github.com/cosmos/cosmos-sdk/types"
 )
 
+type Header struct {
+	abci.Header
+	SealedSeedHash1 []byte
+	SealedSeedHash2 []byte
+	SealedSeedHash3 []byte
+	SealedSeedHash4 []byte
+	SealedSeedHash5 []byte
+	SeedHash1       []byte
+	SeedHash2       []byte
+	SeedHash3       []byte
+	SeedHash4       []byte
+	SeedHash5       []byte
+}
+
 func (c Context) BlockHeader() abci.Header {
 	return c.Value(contextKeyBlockHeader).(abci.Header)
 }
