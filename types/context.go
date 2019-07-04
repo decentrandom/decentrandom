ea9024acfa1e5fa508971e0b738ffafe0125dcd3
decentrandom <misskiwi@decentrandom.com>
2019-07-04 13:43:29 +0900

    Update context.go

--- types/context.go
+++ types/context.go
@@ -1,9 +1,17 @@
 package types
 
 import (
+	"github.com/golang/protobuf/proto"
+	abci "github.com/tendermint/tendermint/abci/types"
+
 	"github.com/cosmos/cosmos-sdk/types"
 )
 
 func (c Context) BlockHeader() abci.Header {
 	return c.Value(contextKeyBlockHeader).(abci.Header)
+}
+
+func (c Context) WithBlockHeader(header abci.Header) Context {
+	var _ proto.Message = &header // for cloning.
+	return c.withValue(contextKeyBlockHeader, header)
 }
