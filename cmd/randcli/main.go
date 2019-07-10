d8d314a0a2ae824e2e3ff1cdcf1efc1e9fbd6c87
DecentRandom <misskiwi@decentrandom.com>
2019-07-11 03:16:23 +0900

    Update main.go

--- cmd/randcli/main.go
+++ cmd/randcli/main.go
@@ -1,134 +1,106 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
 	"path"
-
-	"github.com/rakyll/statik/fs"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-
-	"github.com/decentrandom/decentrandom/app"
-	_ "github.com/decentrandom/decentrandom/client/lcd/statik"
-	"github.com/decentrandom/decentrandom/types/util"
-	"github.com/decentrandom/decentrandom/version"
-	"github.com/decentrandom/decentrandom/x/rand"
-	randClient "github.com/decentrandom/decentrandom/x/rand/client"
-	randrest "github.com/decentrandom/decentrandom/x/rand/client/rest"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/client/lcd"
 	"github.com/cosmos/cosmos-sdk/client/rpc"
-	"github.com/cosmos/cosmos-sdk/client/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	at "github.com/cosmos/cosmos-sdk/x/auth"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	auth "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
-	bank "github.com/cosmos/cosmos-sdk/x/bank/client/rest"
-	crisisclient "github.com/cosmos/cosmos-sdk/x/crisis/client"
-	distcmd "github.com/cosmos/cosmos-sdk/x/distribution"
-	distClient "github.com/cosmos/cosmos-sdk/x/distribution/client"
-	dist "github.com/cosmos/cosmos-sdk/x/distribution/client/rest"
-	sl "github.com/cosmos/cosmos-sdk/x/slashing"
-	slashingClient "github.com/cosmos/cosmos-sdk/x/slashing/client"
-	slashing "github.com/cosmos/cosmos-sdk/x/slashing/client/rest"
-	st "github.com/cosmos/cosmos-sdk/x/staking"
-	stakingClient "github.com/cosmos/cosmos-sdk/x/staking/client"
-	staking "github.com/cosmos/cosmos-sdk/x/staking/client/rest"
 
+	app "github.com/decentrandom/decentrandom"
+	randtypes "github.com/decentrandom/decentrandom/type/util"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	amino "github.com/tendermint/go-amino"
 	"github.com/tendermint/tendermint/libs/cli"
 )
 
-// main -
+const (
+	storeAcc  = "acc"
+	storeRand = "rand"
+)
+
 func main() {
 	cobra.EnableCommandSorting = false
 
 	cdc := app.MakeCodec()
 
+	// Read in the configuration file for the sdk
 	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(util.Bech32PrefixAccAddr, util.Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(util.Bech32PrefixValAddr, util.Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(util.Bech32PrefixConsAddr, util.Bech32PrefixConsPub)
+	config.SetBech32PrefixForAccount(randtypes.Bech32PrefixAccAddr, randtypes.Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(randtypes.Bech32PrefixValAddr, randtypes.Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(randtypes.Bech32PrefixConsAddr, randtypes.Bech32PrefixConsPub)
 	config.Seal()
-
-	mc := []sdk.ModuleClients{
-		distClient.NewModuleClient(distcmd.StoreKey, cdc),
-		stakingClient.NewModuleClient(st.StoreKey, cdc),
-		slashingClient.NewModuleClient(sl.StoreKey, cdc),
-		randClient.NewModuleClient(rand.StoreKey, cdc),
-		crisisclient.NewModuleClient(sl.StoreKey, cdc),
-	}
 
 	rootCmd := &cobra.Command{
 		Use:   "randcli",
-		Short: "DecentRandom 명령어 입력기",
+		Short: "decentrandom Client",
 	}
 
-	rootCmd.PersistentFlags().String(client.FlagChainID, "", "텐더민트 노드의 체인 아이디")
+	// Add --chain-id to persistent flags and mark it required
+	rootCmd.PersistentFlags().String(client.FlagChainID, "", "Chain ID of tendermint node")
 	rootCmd.PersistentPreRunE = func(_ *cobra.Command, _ []string) error {
 		return initConfig(rootCmd)
 	}
 
+	// Construct Root Command
 	rootCmd.AddCommand(
 		rpc.StatusCommand(),
 		client.ConfigCmd(app.DefaultCLIHome),
-		queryCmd(cdc, mc),
-		txCmd(cdc, mc),
+		queryCmd(cdc),
+		txCmd(cdc),
 		client.LineBreak,
 		lcd.ServeCommand(cdc, registerRoutes),
 		client.LineBreak,
 		keys.Commands(),
 		client.LineBreak,
-		version.VersionCmd,
-		client.NewCompletionCmd(rootCmd, true),
 	)
 
-	executor := cli.PrepareMainCmd(rootCmd, "DR", app.DefaultCLIHome)
+	executor := cli.PrepareMainCmd(rootCmd, "Rand", app.DefaultCLIHome)
 	err := executor.Execute()
 	if err != nil {
-		fmt.Printf("명령어 실행 실패 : %s, 종료합니다...\n", err)
-		os.Exit(1)
+		panic(err)
 	}
 }
 
-// queryCmd -
-func queryCmd(cdc *amino.Codec, mc []sdk.ModuleClients) *cobra.Command {
+func registerRoutes(rs *lcd.RestServer) {
+	client.RegisterRoutes(rs.CliCtx, rs.Mux)
+	app.ModuleBasics.RegisterRESTRoutes(rs.CliCtx, rs.Mux)
+}
+
+func queryCmd(cdc *amino.Codec) *cobra.Command {
 	queryCmd := &cobra.Command{
 		Use:     "query",
 		Aliases: []string{"q"},
-		Short:   "쿼리 하부 명령어",
+		Short:   "Querying subcommands",
 	}
 
 	queryCmd.AddCommand(
+		authcmd.GetAccountCmd(cdc),
+		client.LineBreak,
 		rpc.ValidatorCommand(cdc),
 		rpc.BlockCommand(),
-		tx.SearchTxCmd(cdc),
-		tx.QueryTxCmd(cdc),
+		authcmd.QueryTxsByTagsCmd(cdc),
+		authcmd.QueryTxCmd(cdc),
 		client.LineBreak,
-		authcmd.GetAccountCmd(at.StoreKey, cdc),
 	)
 
-	for _, m := range mc {
-		mQueryCmd := m.GetQueryCmd()
-		if mQueryCmd != nil {
-			queryCmd.AddCommand(mQueryCmd)
-		}
-	}
+	// add modules' query commands
+	app.ModuleBasics.AddQueryCommands(queryCmd, cdc)
 
 	return queryCmd
 }
 
-// txCmd -
-func txCmd(cdc *amino.Codec, mc []sdk.ModuleClients) *cobra.Command {
+func txCmd(cdc *amino.Codec) *cobra.Command {
 	txCmd := &cobra.Command{
 		Use:   "tx",
-		Short: "트랜잭션 하부 명령어",
+		Short: "Transactions subcommands",
 	}
 
 	txCmd.AddCommand(
@@ -136,51 +108,18 @@ func txCmd(cdc *amino.Codec, mc []sdk.ModuleClients) *cobra.Command {
 		client.LineBreak,
 		authcmd.GetSignCommand(cdc),
 		authcmd.GetMultiSignCommand(cdc),
-		tx.GetBroadcastCommand(cdc),
-		tx.GetEncodeCommand(cdc),
+		client.LineBreak,
+		authcmd.GetBroadcastCommand(cdc),
+		authcmd.GetEncodeCommand(cdc),
 		client.LineBreak,
 	)
 
-	for _, m := range mc {
-		txCmd.AddCommand(m.GetTxCmd())
-	}
+	// add modules' tx commands
+	app.ModuleBasics.AddTxCommands(txCmd, cdc)
 
 	return txCmd
 }
 
-// CLIVersionRequestHandler -
-func CLIVersionRequestHandler(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write([]byte(fmt.Sprintf("{\"version\": \"%s\"}", version.Version)))
-}
-
-// registerRoutes -
-func registerRoutes(rs *lcd.RestServer) {
-
-	rs.Mux.HandleFunc("/version", CLIVersionRequestHandler).Methods("GET")
-	registerSwaggerUI(rs)
-
-	rpc.RegisterRoutes(rs.CliCtx, rs.Mux)
-	tx.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
-	auth.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, at.StoreKey)
-	bank.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
-	dist.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, distcmd.StoreKey)
-	staking.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
-	slashing.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
-	randrest.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rand.StoreKey)
-}
-
-// registerSwaggerUI -
-func registerSwaggerUI(rs *lcd.RestServer) {
-	statikFS, err := fs.New()
-	if err != nil {
-		panic(err)
-	}
-	staticServer := http.FileServer(statikFS)
-	rs.Mux.PathPrefix("/swagger-ui/").Handler(http.StripPrefix("/swagger-ui/", staticServer))
-}
-
-// initConfig -
 func initConfig(cmd *cobra.Command) error {
 	home, err := cmd.PersistentFlags().GetString(cli.HomeFlag)
 	if err != nil {
