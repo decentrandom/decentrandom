6ae82648a27f1e3da80e595ecfe34e6862696b4f
DecentRandom <misskiwi@decentrandom.com>
2019-07-15 15:40:11 +0900

    Update module.go

--- x/rand/module.go
+++ x/rand/module.go
@@ -24,22 +24,25 @@ var (
 	_ module.AppModuleBasic = AppModuleBasic{}
 )
 
-// app module Basics object
+// AppModuleBasic -
 type AppModuleBasic struct{}
 
+// Name -
 func (AppModuleBasic) Name() string {
 	return ModuleName
 }
 
+// RegisterCodec -
 func (AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
 	RegisterCodec(cdc)
 }
 
+// DefaultGenesis -
 func (AppModuleBasic) DefaultGenesis() json.RawMessage {
 	return ModuleCdc.MustMarshalJSON(DefaultGenesisState())
 }
 
-// Validation check of the Genesis
+// ValidateGenesis -
 func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	var data GenesisState
 	err := ModuleCdc.UnmarshalJSON(bz, &data)
@@ -50,28 +53,29 @@ func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	return ValidateGenesis(data)
 }
 
-// Register rest routes
+// RegisterRESTRoutes -
 func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, rtr *mux.Router) {
 	rest.RegisterRoutes(ctx, rtr, StoreKey)
 }
 
-// Get the root query command of this module
+// GetQueryCmd - Get the root query command of this module
 func (AppModuleBasic) GetQueryCmd(cdc *codec.Codec) *cobra.Command {
 	return cli.GetQueryCmd(StoreKey, cdc)
 }
 
-// Get the root tx command of this module
+// GetTxCmd - Get the root tx command of this module
 func (AppModuleBasic) GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	return cli.GetTxCmd(StoreKey, cdc)
 }
 
+// AppModule -
 type AppModule struct {
 	AppModuleBasic
 	keeper     Keeper
 	coinKeeper bank.Keeper
 }
 
-// NewAppModule creates a new AppModule Object
+// NewAppModule - creates a new AppModule Object
 func NewAppModule(k Keeper, bankKeeper bank.Keeper) AppModule {
 	return AppModule{
 		AppModuleBasic: AppModuleBasic{},
@@ -80,41 +84,52 @@ func NewAppModule(k Keeper, bankKeeper bank.Keeper) AppModule {
 	}
 }
 
+// Name -
 func (AppModule) Name() string {
 	return ModuleName
 }
 
+// RegisterInvariants -
 func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {}
 
+// Route -
 func (am AppModule) Route() string {
 	return RouterKey
 }
 
+// NewHandler -
 func (am AppModule) NewHandler() sdk.Handler {
 	return NewHandler(am.keeper)
 }
+
+// QuerierRoute -
 func (am AppModule) QuerierRoute() string {
 	return ModuleName
 }
 
+// NewQuerierHandler -
 func (am AppModule) NewQuerierHandler() sdk.Querier {
 	return NewQuerier(am.keeper)
 }
 
+// BeginBlock -
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) sdk.Tags {
 	return sdk.EmptyTags()
 }
 
+// EndBlock -
 func (am AppModule) EndBlock(sdk.Context, abci.RequestEndBlock) ([]abci.ValidatorUpdate, sdk.Tags) {
 	return []abci.ValidatorUpdate{}, sdk.EmptyTags()
 }
 
+// InitGenesis -
 func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState GenesisState
 	ModuleCdc.MustUnmarshalJSON(data, &genesisState)
 	return InitGenesis(ctx, am.keeper, genesisState)
 }
 
+// ExportGenesis -
 func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
 	return ModuleCdc.MustMarshalJSON(gs)
