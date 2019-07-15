356846492a7861ec7436c8ac97f4baa4becda683
DecentRandom <misskiwi@decentrandom.com>
2019-07-15 15:40:09 +0900

    Update genesis.go

--- x/rand/genesis.go
+++ x/rand/genesis.go
@@ -7,39 +7,45 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
+// GenesisState -
 type GenesisState struct {
 	RoundRecords []Round `json:"round_records"`
 }
 
+// NewGenesisState -
 func NewGenesisState(roundRecords []Round) GenesisState {
 	return GenesisState{RoundRecords: nil}
 }
 
+// ValidateGenesis -
 func ValidateGenesis(data GenesisState) error {
 	for _, record := range data.RoundRecords {
 		if record.Owner == nil {
-			return fmt.Errorf("Invalid RoundRecord: Value: %s. Error: Missing Owner", record.Id)
+			return fmt.Errorf("Invalid RoundRecord: Value: %s. Error: Missing Owner", record.ID)
 		}
 	}
 	return nil
 }
 
+// DefaultGenesisState -
 func DefaultGenesisState() GenesisState {
 	return GenesisState{
 		RoundRecords: []Round{},
 	}
 }
 
+// InitGenesis -
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) []abci.ValidatorUpdate {
 	for _, record := range data.RoundRecords {
-		keeper.SetRound()(ctx, record.Id, record)
+		keeper.SetRound(ctx, record.ID, record)
 	}
 	return []abci.ValidatorUpdate{}
 }
 
+// ExportGenesis -
 func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 	var records []Round
-	iterator := k.GetRoundsIterator(ctx)
+	iterator := k.GetIDsIterator(ctx)
 	for ; iterator.Valid(); iterator.Next() {
 		id := string(iterator.Key())
 		var round Round
