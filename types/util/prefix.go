00cf8c3ca6ff9f5ac05f0b754dd7266b66a4a751
DecentRandom <misskiwi@decentrandom.com>
2019-07-16 09:28:03 +0900

    Update prefix.go

--- types/util/prefix.go
+++ types/util/prefix.go
@@ -1,16 +1,16 @@
 package util
 
 const (
-	// Bech32PrefixAccAddr - 기본적인 계정 주소
+	// Bech32PrefixAccAddr -
 	Bech32PrefixAccAddr = "rand"
-	// Bech32PrefixAccPub - 계정의 공개 키
+	// Bech32PrefixAccPub -
 	Bech32PrefixAccPub = "randpub"
-	// Bech32PrefixValAddr - 검증인 오퍼레이터 주소
+	// Bech32PrefixValAddr -
 	Bech32PrefixValAddr = "randvaloper"
-	// Bech32PrefixValPub - 검증인 오퍼레이터의 공개 키
+	// Bech32PrefixValPub -
 	Bech32PrefixValPub = "randvaloperpub"
-	// Bech32PrefixConsAddr - 합의 노드 주소
+	// Bech32PrefixConsAddr -
 	Bech32PrefixConsAddr = "randvalcons"
-	// Bech32PrefixConsPub - 합의 노드의 공개 키
+	// Bech32PrefixConsPub -
 	Bech32PrefixConsPub = "randvalconspub"
 )
