eaa387cc45e6777cdbe55851d3b2fa3c7a48b9cf
DecentRandom <misskiwi@decentrandom.com>
2019-07-23 15:07:51 +0900

    Update types.go

--- x/rand/client/types/types.go
+++ x/rand/client/types/types.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"time"
 	"fmt"
 	"strings"
 
@@ -15,25 +16,4 @@ type Round struct {
 	NonceHash     string         `json:"nonce_hash"`
 	Targets       []string       `json:"targets"`
 	ScheduledTime time.Time      `json:"scheduled_time"`
-}
-
-func (a Artist) String() string {
-	return fmt.Sprintf(`Song %d:
-		  Artist ID:	%s
-		  Image:		%s
-		  Name:			%s`, a.ArtistID, a.Image, a.Name)
-}
-
-// Artists is an array of song
-// To FIX with new fields
-type Artists []*Artist
-
-func (artists Artists) String() string {
-	out := fmt.Sprintf("%10s - (%15s) - (%40s) - [%10s] - Create Time\n", "ID", "Title", "Owner", "CreateTime")
-	for _, artist := range artists {
-		out += fmt.Sprintf("%10d - (%15s) - (%40s) - [%10s]\n",
-			artist.ArtistID, artist.Image, artist.Name)
-	}
-
-	return strings.TrimSpace(out)
 }
