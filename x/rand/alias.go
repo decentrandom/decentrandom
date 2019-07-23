cf65815c879850ecb5b4878ef348e005ae5e8d57
DecentRandom <misskiwi@decentrandom.com>
2019-07-23 15:15:31 +0900

    migration

---
+++ x/rand/alias.go
@@ -0,0 +1,19 @@
+package rand
+
+import (
+	"github.com/decentrandom/decentrandom/x/rand/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	RouterKey  = types.RouterKey
+	StoreKey   = types.StoreKey
+)
+
+var (
+	RegisterCodec = types.RegisterCodec
+)
+
+type (
+	Round = types.Round
+)
--- x/rand/types/codec.go
+++ x/rand/types/codec.go
@@ -1,10 +1,15 @@
-package rand
+package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-// RegisterCodec -
+var ModuleCdc = codec.New()
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}
+
 func RegisterCodec(cdc *codec.Codec) {
 
 	cdc.RegisterConcrete(MsgNewRound{}, "rand/NewRound", nil)
