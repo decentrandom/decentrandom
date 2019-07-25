e2642d2351fb5eb3fcf68862bbc953b10cdb1970
decentrandom <misskiwi@decentrandom.com>
2019-07-25 16:26:36 +0900

    Update utils.go

--- app/utils.go
+++ app/utils.go
@@ -1,12 +1,9 @@
 package app
 
 import (
-	"io"
-
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/libs/log"
 
-	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 )
@@ -14,10 +11,8 @@ import (
 // DONTCOVER
 
 // NewRandAppUNSAFE -
-func NewRandAppUNSAFE(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bool,
-	invCheckPeriod uint, baseAppOptions ...func(*bam.BaseApp),
-) (rapp *RandApp, keyMain, keyStaking *sdk.KVStoreKey, stakingKeeper staking.Keeper) {
+func NewRandAppUNSAFE(logger log.Logger, db dbm.DB, invCheckPeriod uint) (rapp *RandApp, keyMain, keyStaking *sdk.KVStoreKey, stakingKeeper staking.Keeper) {
 
-	rapp = NewRandApp(logger, db, traceStore, loadLatest, invCheckPeriod, baseAppOptions...)
+	rapp = NewRandApp(logger, db, invCheckPeriod)
 	return rapp, rapp.keyMain, rapp.keyStaking, rapp.stakingKeeper
 }
