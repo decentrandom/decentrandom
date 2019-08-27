690214405c97284969e7d6661f05b0abbe3947df
decentrandom <misskiwi@decentrandom.com>
2019-08-28 04:34:49 +0900

    Update query.go

--- x/rand/client/cli/query.go
+++ x/rand/client/cli/query.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"encoding/hex"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -10,6 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/tendermint/tendermint/crypto/tmhash"
 )
 
 // GetQueryCmd -
@@ -24,6 +26,7 @@ func GetQueryCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	randQueryCmd.AddCommand(client.GetCommands(
 		GetCmdRoundInfo(cdc),
 		GetCmdRoundIDs(cdc),
+		GetCmdHashNonce(cdc),
 	)...)
 	return randQueryCmd
 }
@@ -70,6 +73,41 @@ func GetCmdRoundIDs(cdc *codec.Codec) *cobra.Command {
 
 			return cliCtx.PrintOutput(out)
 
+		},
+	}
+}
+
+// GetCmdHashNonce -
+func GetCmdHashNonce(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "hash-nonce [nonce]",
+		Short: "get hash string from a nonce",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			nonce := args[0]
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/hash_nonce/%s", types.QuerierRoute, nonce), nil)
+			if err != nil {
+				return nil
+			}
+
+			// Hashing Nonce
+			hasher := tmhash.New()
+			nonceVector := []byte(args[1])
+			_, hashError := hasher.Write(nonceVector)
+			if hashError != nil {
+				return hashError
+			}
+			bz := tmhash.Sum(nonceVector)
+			nonceHash := hex.EncodeToString(bz)
+
+			var nonceStruct types.Nonce
+
+			nonceStruct = types.Nonce{nonce, nonceHash}
+
+			cdc.MustUnmarshalJSON(res, &nonceStruct)
+			return cliCtx.PrintOutput(nonceStruct)
 		},
 	}
 }
