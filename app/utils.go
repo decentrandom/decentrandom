fa737a17e4225178408aa358e7f79b4e0cd2880a
decentrandom <misskiwi@decentrandom.com>
2019-09-06 16:51:23 +0900

    Update utils.go

--- app/utils.go
+++ app/utils.go
@@ -14,5 +14,5 @@ import (
 func NewRandAppUNSAFE(logger log.Logger, db dbm.DB, invCheckPeriod uint) (rapp *RandApp, keyMain, keyStaking *sdk.KVStoreKey, stakingKeeper staking.Keeper) {
 
 	rapp = NewRandApp(logger, db, invCheckPeriod)
-	return rapp, rapp.keyMain, rapp.keys[staking.StoreKey], rapp.stakingKeeper
+	return rapp, rapp.keys[bam.MainStoreKey], rapp.keys[staking.StoreKey], rapp.stakingKeeper
 }
