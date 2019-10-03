8a73c10ce21ab53d761d94d90d8d22176379ad09
decentrandom <misskiwi@decentrandom.com>
2019-10-03 15:15:09 +0900

    Update keeper.go

--- x/rand/internal/keeper/keeper.go
+++ x/rand/internal/keeper/keeper.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/decentrandom/decentrandom/x/rand/internal/types"
 )
 
 // Keeper -
