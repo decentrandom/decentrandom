b621b4dd417972e5e46241d0e23c6f290236e118
decentrandom <misskiwi@decentrandom.com>
2019-10-23 17:52:39 +0900

    Update abci.go

--- x/rand/abci.go
+++ x/rand/abci.go
@@ -2,6 +2,7 @@ package rand
 
 import (
 	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -9,4 +10,8 @@ import (
 func EndBlocker(ctx sdk.Context, k Keeper) {
 
 	fmt.Println()
+	fmt.Printf("RAND Deposit")
+	fmt.Println()
+
+	// TODO : process RAND depost
 }
