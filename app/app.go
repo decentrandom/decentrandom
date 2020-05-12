3023eb0512de6f1fbd0a4cc4785d47348e025bea
decentrandom <misskiwi@decentrandom.com>
2020-05-12 16:53:53 +0900

    Update app.go

--- app/app.go
+++ app/app.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	abci "github.com/tendermint/tendermint/abci/types"
-	tmos "github.com/tendermint/tendermint/libs/os"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
@@ -371,7 +371,7 @@ func NewRandApp(logger log.Logger, db dbm.DB, invCheckPeriod uint) *RandApp {
 
 	err := app.LoadLatestVersion(keys[bam.MainStoreKey])
 	if err != nil {
-		tmos.Exit(err.Error())
+		cmn.Exit(err.Error())
 	}
 
 	return app
