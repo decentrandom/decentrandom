dcb86141cff0f287f27213e10c219b5d46d4f8d9
decentrandom <misskiwi@decentrandom.com>
2020-12-04 04:04:59 +0900

    Update app.go

--- app/app.go
+++ app/app.go
@@ -6,8 +6,8 @@ import (
 	"os"
 
 	abci "github.com/tendermint/tendermint/abci/types"
-	cmn "github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/log"
+	tmos "github.com/tendermint/tendermint/libs/os"
 	dbm "github.com/tendermint/tm-db"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
@@ -360,18 +360,9 @@ func NewRandApp(logger log.Logger, db dbm.DB, invCheckPeriod uint) *RandApp {
 		auth.DefaultSigVerificationGasConsumer))
 	app.SetEndBlocker(app.EndBlocker)
 
-	/*
-		if loadLatest {
-			err := app.LoadLatestVersion(app.keyMain)
-			if err != nil {
-				cmn.Exit(err.Error())
-			}
-		}
-	*/
-
 	err := app.LoadLatestVersion(keys[bam.MainStoreKey])
 	if err != nil {
-		cmn.Exit(err.Error())
+		tmos.Exit(err.Error())
 	}
 
 	return app
